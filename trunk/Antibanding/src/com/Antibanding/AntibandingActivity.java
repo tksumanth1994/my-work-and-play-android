a8b90fc7acb5d6cfca804b44232a712992679506
y38y38@gmail.com <y38y38@gmail.com@537f935e-516b-1b86-98de-bd934649554d>
2011-10-09 13:49:13 +0000

    add setDisplayOrientation

--- trunk/Antibanding/src/com/Antibanding/AntibandingActivity.java
+++ trunk/Antibanding/src/com/Antibanding/AntibandingActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.hardware.Camera;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.Window;
@@ -17,8 +18,11 @@ import android.view.WindowManager;
 public class AntibandingActivity extends Activity {
     /** Called when the activity is first created. */
     Camera _camera;
+    Activity _activity;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	_activity = this;
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -43,7 +47,32 @@ public class AntibandingActivity extends Activity {
     	
 		return super.onTouchEvent(event); 
 	}    
-    
+    public void setCameraDisplayOrientation(android.hardware.Camera camera, int cameraId) {
+        android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+        
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        
+        int rotation = _activity.getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        
+		Log.d("camera", "result= " + result);
+		camera.setDisplayOrientation(result);
+    }
+ 
     public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
         private SurfaceHolder holder;
      
@@ -71,6 +100,7 @@ public class AntibandingActivity extends Activity {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
+			setCameraDisplayOrientation(_camera, 0);
 			_camera.startPreview();
 
 			
